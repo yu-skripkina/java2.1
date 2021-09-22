dfdd6e641c640841419be493e5716c1610665b1b
Yuliya Skripkina <yu.skripkina@gmail.com>
2021-09-22 23:23:45 +0300

    Added Main.java

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+        public static void main(String[] args) {
+            int amount = 999 ;
+            int bonusPrise = 20 ;
+            int miles = amount / bonusPrise ;
+            System.out.println ("Количество миль:" +miles) ;
+
+
+        }
+}
